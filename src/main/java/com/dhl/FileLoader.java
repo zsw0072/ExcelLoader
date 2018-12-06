3a54b523da7f3466dbb6a8859727a5c7b81332fd
itheima <bos@itheima.com>
2018-12-07 07:32:31 +0800

    commit 1.0

--- src/main/java/com/dhl/FileLoader.java
+++ src/main/java/com/dhl/FileLoader.java
@@ -12,6 +12,7 @@ import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileLoader {
@@ -20,23 +21,22 @@ public class FileLoader {
         BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\arfa\\Desktop\\dhl\\1.txt"));
         HSSFWorkbook xssfWorkbook = new HSSFWorkbook();
         HSSFSheet sheet = xssfWorkbook.createSheet("2018-08");
-        FileOutputStream fos = new FileOutputStream("C:\\Users\\arfa\\Desktop\\dhl\\201808.xlsx");
+        FileOutputStream fos = new FileOutputStream("C:\\Users\\arfa\\Desktop\\dhl\\201808.xls");
         int count =0;
         String line;
-        HSSFCell cell;
         while((line=br.readLine())!=null){
-            String[] strings = line.split("\\|");
             HSSFRow row = sheet.createRow(count);
+            String[] strings = line.split("\\|");
             for (int i = 0; i < strings.length; i++) {
-
-                cell = row.createCell(i);
+                HSSFCell cell = row.createCell(i);
                 cell.setCellType(HSSFCell.CELL_TYPE_STRING);
                 cell.setCellValue(strings[i]);
-                xssfWorkbook.write(fos);
-
+                fos.flush();
             }
+            xssfWorkbook.write(fos);
             count++;
         }
+        fos.close();
         br.close();
     }
 }
