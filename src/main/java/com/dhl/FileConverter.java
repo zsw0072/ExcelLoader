249562f1a25e2af4d8b979c1e6f608521721d67a
zsw0072 <arfa.cheung.069@hotmail.com>
2018-12-14 14:11:55 +0800

    1.1 fix the excel filter issue

--- src/main/java/com/dhl/FileConverter.java
+++ src/main/java/com/dhl/FileConverter.java
@@ -14,46 +14,43 @@ import java.util.Scanner;
 
 public class FileConverter {
 
+    private final static String TAGET_FLAG = "taget";
+
     public static void main(String[] args) throws Exception {
-        String source = "C:\\Users\\Admin\\Desktop\\CAO_201811ecom_base.txt";
-        ConvertTxtToExcel(source, source.replace(".txt",".xlsx"),
+        String source = "C:\\Users\\Admin\\Desktop\\test.txt";
+        ConvertTxtToExcel(source, source.replace(".txt", ".xlsx"),
                 "2018-11");
     }
 
     //convert the DHL monthly report to excel file
-    public static void ConvertTxtToExcel(String source,String dest,String sheetName) throws Exception{
-        
+    public static void ConvertTxtToExcel(String source, String dest, String sheetName) throws Exception {
+
         FileInputStream fis = new FileInputStream(source);
-        Scanner sc = new Scanner(fis,"UTF-8");
+        Scanner sc = new Scanner(fis, "UTF-8");
         // keep 100 rows in memory, exceeding rows will be flushed to disk
         SXSSFWorkbook sxssfWorkbook = new SXSSFWorkbook(100);
         Sheet sheet = sxssfWorkbook.createSheet(sheetName);
         FileOutputStream fos = new FileOutputStream(dest);
         String line;
-        int count =0;
-        while(sc.hasNextLine()){
+        int count = 0;
+        while (sc.hasNextLine()) {
             line = sc.nextLine();
             String[] word = line.split("\\|");
             List<String> list = new ArrayList<String>();
             for (int i = 0; i < word.length; i++) {
-                if (i==0||i==2||i==3||i==13||i==21||i==23
-                        ||i==24||i==34||i==35||i==37
-                        ||i==38||i==39||i==40||i==43
-                        ||i==44||i==47||i==49){
-                    word[i] = "taget";
+                if (ColumnList.getList().contains(i)) {
+                    word[i] = TAGET_FLAG;
                 }
                 list.add(word[i]);
-                if (list.contains("taget")){
-                    list.remove("taget");
+                if (list.contains(TAGET_FLAG)) {
+                    list.remove(TAGET_FLAG);
                 }
             }
-
             Row row = sheet.createRow(count);
             for (int j = 0; j < list.size(); j++) {
-                    Cell cell = row.createCell(j);
-                    cell.setCellType(HSSFCell.CELL_TYPE_STRING);
-                    cell.setCellValue(list.get(j));
-
+                Cell cell = row.createCell(j);
+                cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+                cell.setCellValue(list.get(j));
             }
             fos.flush();
             count++;
--- src/main/java/com/dhl/test/TestDemo.java
+++ src/main/java/com/dhl/test/TestDemo.java
@@ -1,5 +1,7 @@
 package com.dhl.test;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,5 +18,14 @@ public class TestDemo {
         if(list.contains(j)){
             list.remove(j);
         }
+    }
+
+    @Test
+    public void checklist(){
+        int[] intArr = {0,1,2,3};
+        int i = 2;
+        String s = intArr.toString();
+        System.out.println(s);
+
     }
 }
